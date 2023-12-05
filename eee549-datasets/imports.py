3d3b22d7b38650b7ea8efe59d13a157e92fa421b
vivek <viveksahukar@gmail.com>
2023-12-05 09:23:12 -0700

    uploaded final code files except UCI dataset

--- eee549-datasets/imports.py
+++ eee549-datasets/imports.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.datasets import load_breast_cancer, fetch_openml
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score, make_scorer, roc_auc_score, roc_curve, average_precision_score, precision_recall_curve, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, auc, make_scorer, roc_auc_score, roc_curve, average_precision_score, precision_recall_curve, classification_report, confusion_matrix
 from sklearn.utils import resample
 from sklearn.pipeline import Pipeline
 from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+# from sklearn.svm import SVC
+# import cudf
 # from sklearnex import patch_sklearn
 # patch_sklearn()
-# from sklearn.svm import SVC
